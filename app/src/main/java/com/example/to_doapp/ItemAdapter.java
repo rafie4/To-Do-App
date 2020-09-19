c2d5f177c2a3914b49961064daf251d36ee78938
Elisee Djapa <eliseedjapa@gmail.com>
2020-09-19 11:56:23 -0400

    Initial commit

---
+++ app/src/main/java/com/example/to_doapp/ItemAdapter.java
@@ -0,0 +1,92 @@
+package com.example.to_doapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+//Responsible for displaying the data for the model into a row in the recycle view
+public class ItemAdapter extends  RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+
+    public interface OnLongClickListener{
+        void onItemLongClicked(int position);
+
+    }
+
+    List<String> items;
+    OnLongClickListener longClickListener;
+
+
+
+    public ItemAdapter(List<String> items,   OnLongClickListener longClickListener) {
+        this.items = items;
+        this.longClickListener = longClickListener;
+    }
+
+    @NonNull
+    @Override
+    //Responsible for creating each view
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        //use layout inflator to inflate a view
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+
+
+        //wrap it inside a view holder and return it
+        return new ViewHolder(todoView);
+
+
+    }
+    //responsible for binding data to a particular view holder
+    @Override
+    //Takes data as a position and put into a view holder
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //Grab the item at the position
+        String item = items.get(position);
+
+
+
+        //bind the item into the specified view holder
+        holder.bind(item);
+
+    }
+
+    @Override
+    //Number of items  available in the data
+    //Tells recyler how many items are in the list.
+
+    public int getItemCount() {
+        return items.size();
+    }
+
+    //Container to provide easy access to views that represent each row in the list
+    class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvItem;
+        OnLongClickListener longClickListener;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+
+        }
+//update the view inside of the view holder, with the data of string item
+        public void bind(String item) {
+            tvItem.setText(item);
+            tvItem.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    /* Notify the user about the position that was long pressed */
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    return true;
+                }
+            });
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/to_doapp/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.to_doapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+
+    Button btnAddTask;
+    EditText editItem;
+    RecyclerView rvItems;
+    ItemAdapter itemsAdapter;
+
+    //@SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAddTask = findViewById(R.id.btnAddTask);
+        rvItems = findViewById(R.id.rvItems);
+        editItem = findViewById(R.id.editItem);
+
+
+
+       // editItem.setText("I'm doing this from Android Studio!");
+
+        items = new ArrayList<>();
+//        items.add("Buy Milk");
+//        items.add("Call Gran Gran!");
+//        items.add("Pause, Appreciate life!");
+
+        ItemAdapter.OnLongClickListener onLongClickListener =  new ItemAdapter.OnLongClickListener (){
+            @Override
+            public void onItemLongClicked(int position) {
+                //Delete the item from the model or screen
+                items.remove(position);
+                //Notify the adapter of the deleted position
+                itemsAdapter.notifyItemRemoved(position);
+                Toast.makeText(getApplicationContext(), "Task has been removed!", Toast.LENGTH_SHORT).show();
+
+            }
+        };
+        final ItemAdapter itemsAdapter = new ItemAdapter(items, onLongClickListener);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+
+        //Implementing Add button logic
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoItem = editItem.getText().toString();
+                //Add item to the model
+                items.add(todoItem);
+                //Modify the adapter that an item is inserted
+                itemsAdapter.notifyItemInserted(items.size() -1);
+                editItem.setText("");
+                Toast.makeText(getApplicationContext(), "Task has been added!", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getBaseContext(), "Reason can not be blank", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
