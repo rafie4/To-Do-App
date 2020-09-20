8116afda669576cb85afabe407f4bc66be2d9dd7
Elisee Djapa <eliseedjapa@gmail.com>
2020-09-20 18:48:38 -0400

    MVP Task

--- app/src/main/java/com/example/to_doapp/MainActivity.java
+++ app/src/main/java/com/example/to_doapp/MainActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+//import android.os.FileUtils;
+import org.apache.commons.io.FileUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,10 +39,11 @@ public class MainActivity extends AppCompatActivity {
 
        // editItem.setText("I'm doing this from Android Studio!");
 
-        items = new ArrayList<>();
-        items.add("Buy Milk");
-        items.add("Call Gran Gran!");
-        items.add("Pause, Appreciate life!");
+        loadItems();
+//        items = new ArrayList<>();
+//        items.add("Buy Milk");
+//        items.add("Call Gran Gran!");
+//        items.add("Pause, Appreciate life!");
 
         ItemAdapter.OnLongClickListener onLongClickListener =  new ItemAdapter.OnLongClickListener (){
             @Override
@@ -49,9 +53,11 @@ public class MainActivity extends AppCompatActivity {
                 //Notify the adapter of the deleted position
                 itemsAdapter.notifyItemRemoved(position);
                 Toast.makeText(getApplicationContext(), "Task has been removed!", Toast.LENGTH_SHORT).show();
+                saveItems();
             }
         };
-        final ItemAdapter itemsAdapter = new ItemAdapter(items, onLongClickListener);
+
+        itemsAdapter = new ItemAdapter(items, onLongClickListener);
         rvItems.setAdapter(itemsAdapter);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
 
@@ -68,9 +74,38 @@ public class MainActivity extends AppCompatActivity {
                 editItem.setText("");
                 Toast.makeText(getApplicationContext(), "Task has been added!", Toast.LENGTH_SHORT).show();
                 //Toast.makeText(getBaseContext(), "Reason can not be blank", Toast.LENGTH_SHORT).show();
+                saveItems();
 
             }
         });
+
+    }
+
+
+    private File getdataFile(){
+        return new File(getFilesDir(), "data.txt");
+    }
+
+    //This function will load items by reading every line of the data file
+    private void loadItems(){
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getdataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            //log.e("MainActivity", "Error reading items", e);
+            items = new ArrayList<>();
+        }
+
+    }
+
+    //This function saves items by writing them to the data file
+    private void saveItems(){
+        try {
+            FileUtils.writeLines(getdataFile(), items);
+        } catch (IOException e) {
+            e.printStackTrace();
+           // log.e("MainActivity", "Error Writing items", e);
+        }
 
     }
 }
